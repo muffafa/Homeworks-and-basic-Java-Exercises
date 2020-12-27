d1b5d1d58dab2cc861130d3460d81c7600bf64c5
muffafa <62511949+muffafa@users.noreply.github.com>
2020-12-27 07:34:30 +0300

    Update and rename nld.java to Nld.java

--- Nld.java
+++ Nld.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class nld {
+public class Nld {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		System.out.print("Number of rows? ");
