41daf77ec6e7d366c3c9d83f719755548c142350
muffafa <62511949+muffafa@users.noreply.github.com>
2021-01-24 22:58:53 +0300

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Palindrome {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a string: ");
+    String s = input.nextLine();
+    
+    int low = 0;
+    int high = s.length() - 1;
+
+    boolean isPalindrome = true;
+
+    while(low<high){
+      if(s.charAt(low) != s.charAt(high)){
+        isPalindrome = false;
+        break;
+      }
+      low++;
+      high--;
+    }
+    if(isPalindrome)
+      System.out.println(s+" is a palindrome");
+    else 
+      System.out.println(s+" not a palindrome");
+  }
+}
