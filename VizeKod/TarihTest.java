8af10b81967ba7a9dba33568213d7a1d096f9b7a
muffafa <62511949+muffafa@users.noreply.github.com>
2021-04-18 18:41:59 +0300

    Add files via upload

---
+++ VizeKod/TarihTest.java
@@ -0,0 +1,13 @@
+package VizeKod;
+
+public class TarihTest {
+
+	public static void main(String[] args) {
+		Tarih tarih1 = new Tarih(23,4,1920);
+		Tarih tarih2 = new Tarih(30,8,1922);
+		
+		System.out.println(tarih1.compareTo(tarih2));
+		
+	}
+
+}
